cfc52cdc45cdb9f8a2d076fe1175babba4d191ad
moradi <moradi@hamta.spos>
2022-12-01 15:42:50 +0330

    add script to install required packages on board

---
+++ board/setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+apt update
+apt install python3-picamera
+apt install build-essential cmake pkg-config
+apt install libx11-dev libatlas-base-dev
+apt install libgtk-3-dev libboost-python-dev
+
+echo "Next command may take few minutes to finish"
+pip3 install dlib
+
+wget http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2
+bunzip2 shape_predictor_5_face_landmarks.dat.bz2
+
+wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+bunzip2 dlib_face_recognition_resnet_model_v1.dat.bz2
+
+mkdir samples
+
+echo "Make sure camera module is enabled (you can access the settings by raspi-config command"
+
