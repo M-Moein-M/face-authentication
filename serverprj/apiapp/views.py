7a98eb2d077c3ba0563c8555a576be6c1f37434b
moradi <moradi@hamta.spos>
2022-12-06 19:40:39 +0330

    add extracting feature from uploaded face

--- serverprj/apiapp/views.py
+++ serverprj/apiapp/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import render
 
 from django.core.files.storage import FileSystemStorage
 
+import dlib
+import numpy as np
 
 class VerificationCheck(APIView):
     """
@@ -41,4 +43,39 @@ class NewFaceRegister(View):
         """ Extract new features and save new record in databse """
         file_name = "new_face.jpg"
         fss = FileSystemStorage()
+        fss.delete(file_name)
         fss.save(file_name, upload)
+
+        feat = self.get_feature(fss.path(file_name))
+        if feat is None:
+            print("No faces detected")
+            return
+
+        print(feat.dtype)
+        print(feat.to)
+        print(feat)
+
+    @staticmethod
+    def get_feature(p):
+        fss = FileSystemStorage()
+
+        detector = dlib.get_frontal_face_detector()
+        sp = dlib.shape_predictor(fss.path('shape_predictor_5_face_landmarks.dat'))
+        facerec = dlib.face_recognition_model_v1(fss.path('dlib_face_recognition_resnet_model_v1.dat'))
+        # Now process captured image
+        img_path = p
+        print("Processing file: {}".format(img_path))
+        img = dlib.load_rgb_image(img_path)
+
+        dets = detector(img, 1)
+        print("Number of faces detected: {}".format(len(dets)))
+
+        # Now process each face we found.
+        for k, d in enumerate(dets):
+            # Get the landmarks/parts for the face in box d.
+            shape = sp(img, d)
+
+            # Compute the 128D vector
+            face_descriptor = facerec.compute_face_descriptor(img, shape)
+            v = np.array(face_descriptor)
+            return v
